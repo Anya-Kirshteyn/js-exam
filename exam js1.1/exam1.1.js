3c8ef5c7391c6838f22819c475eba7042c6998c7
Anya-Kirshteyn <ann65ann@gmail.com>
2025-04-10 17:27:23 +0300

    exam isEmptyDeep

--- exam js1.1/exam1.1.js
+++ exam js1.1/exam1.1.js
@@ -34,6 +34,55 @@
 // IsEmptyDeep. Написать функцию, которая выполняет глубокую проверку объекта на пустоту.
 //     Пустые значения: "", null, NaN, undefined, [].
 //     Ожидаемый результат: ({}) => true, ({ a: { b: undefined } }) => true, ({ a: { b: [] } }) => true.
+//
+// function isEmptyDeep(value) {
+//     if (value === null || value === undefined) {
+//         return true;
+//     }
+//
+//     if (typeof value === 'number' && isNaN(value)) {
+//         return true;
+//     }
+//
+//     if (typeof value === 'string' && value === '') {
+//         return true;
+//     }
+//
+//     if (Array.isArray(value)) {
+//         return value.length === 0 || value.every(isEmptyDeep);
+//     }
+//
+//     if (typeof value === 'object') {
+//         const values = Object.values(value);
+//         return values.length === 0 || values.every(isEmptyDeep);
+//     }
+//
+//     return false;
+// }
+// console.log(isEmptyDeep([[], null,{a:{b:[{},{c:''}]}}]))
+// console.log(isEmptyDeep([[], null,{a:{b:[{},'{}']}}]))
 
 
 
+
+
+
+
+
+
+
+
+
+// Basic Login Form с валидацией email/пароля.
+//
+//     Стек технологий:
+//
+//     React.js или Angular.js;
+//
+// Formik;
+//
+// Любая библиотека для стилей.
+//
+//     Ссылка на макет:
+//     https://www.figma.com/file/0sbQJXtPAATqPYn4euN5KX/
+//         React-Playground-%E2%80%93-Sign-Up-Screen-%F0%9F%94%91-(Community)?node-id=447%3A2
